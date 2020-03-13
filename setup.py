51d2cce6675aae85ec9a728982e5b88cc1ad11f1
pfajardo-nj <paulo.fajardo@nathanjames.com>
2020-03-14 05:57:29 +0800

    Toolbox
    Testing

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name='NathanJamesToolbox',
-      version='0.0.1',
-      description='This package has components used for NJ specific work.',
-      author='Paulo Fajardo',
-      author_email='paulo.fajardo@nathanjames.com',
-      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests", '*.csv']),
-      license='LICENSE.txt')
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='NathanJamesToolbox',
+    version='0.1',
+    scripts=['NathanJamesToolbox'],
+    author="Paulo Fajardo",
+    author_email="paulo.fajardo@nathanjames.com",
+    description="Package for NJ scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pfajardo-nj/NathanJamesToolbox",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
